12e28184806d354cbcb55014dc0da7a3c19993a4
CIST2932Spring2019 <william.ith13@gmail.com>
2019-02-21 19:01:04 -0500

    code before Cross-Origin Resource Sharing (CORS) added

--- PatientsAPI/Startup.cs
+++ PatientsAPI/Startup.cs
@@ -26,12 +26,6 @@ namespace PatientsPage
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            {
-                builder.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            }));
             services.AddScoped<PatientService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -48,8 +42,6 @@ namespace PatientsPage
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseCors("MyPolicy");
 
             app.UseHttpsRedirection();
             app.UseMvc();
